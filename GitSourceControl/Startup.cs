7cede3ec9391fd10e5041ef3f2a75e612939c793
pkcmca <pkcmca@gmail.com>
2017-01-07 11:15:33 -0500

    Created

---
+++ GitSourceControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitSourceControl.Startup))]
+namespace GitSourceControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
